3e1b39b96ca4452d4a8e5eeca9824856619a47ba
roshani2505 <roshanikumari250305@gmail.com>
2025-11-23 18:29:28 +0530

    added question 12

---
+++ Ques12.js
@@ -0,0 +1,7 @@
+////L0 - Square and Cube of a Number with Implicit Return
+const getSquareAndCube = num => ({
+  square: num * num,
+  cube: num * num * num
+});
+
+console.log(getSquareAndCube(5));
